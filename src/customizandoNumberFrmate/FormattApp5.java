587875b26cf0aac75dcefedcf2785c58340fea7e
Rafa <rafa.willian91@gmail.com>
2023-03-01 03:48:38 -0300

    Customizando NumberFormat(set)

---
+++ src/customizandoNumberFrmate/FormattApp5.java
@@ -0,0 +1,27 @@
+package customizandoNumberFrmate;
+
+import java.text.NumberFormat;
+import java.util.Currency;
+
+public class FormattApp5 {
+
+    public static void main(String[] args) {
+
+        double d = 320320323.9998;
+
+        NumberFormat nf = NumberFormat.getInstance();
+        System.out.println(nf.format(d));
+
+        nf.setGroupingUsed(false);
+        System.out.println(nf.format(d));
+
+        nf.setMaximumFractionDigits(7);
+        nf.setMinimumFractionDigits(7);
+        System.out.println(nf.format(d));
+
+        nf.setCurrency(Currency.getInstance("EUR"));//Para ter Suporte a esse set Tenho que passar um NumberFormat.getCurrencyInstance();
+        System.out.println(nf.format(d));
+
+    }
+
+}
